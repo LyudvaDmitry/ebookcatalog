f8b778b033d50be4ca0a7992e6d8354f78939ac3
LyudvaDmitry <lyudva.dmitry@gmail.com>
2015-08-18 17:47:25 +0300

    Reworked cover search

--- ebookcatalog.go
+++ ebookcatalog.go
@@ -54,12 +54,31 @@ type path struct {
 	P string `xml:"full-path,attr"`
 }
 
-type Book struct {
+type RawBook struct {
 	Title       string `xml:"metadata>title"`
 	Creator     string `xml:"metadata>creator"`
 	Subject     string `xml:"metadata>subject"`
 	Description string `xml:"metadata>description"`
 	Language    string `xml:"metadata>language"`
+	Meta    	[]name `xml:"metadata>meta"`
+	Items    	[]item `xml:"manifest>item"`
+}
+
+type name struct {
+	Content string `xml:"content,attr"`
+	Name string `xml:"name,attr"`
+}
+type item struct {
+	Ref string `xml:"href,attr"`
+	Id string `xml:"id,attr"`
+}
+
+type Book struct {
+	Title       string 
+	Creator     string 
+	Subject     string 
+	Description string 
+	Language    string 
 	Path        string
 	Cover       string
 }
@@ -118,40 +137,67 @@ func (bc *Ebookcatalog) extract(path string, info os.FileInfo, err error) error 
 	if err != nil {
 		return err
 	}
-	var b Book
+	var b RawBook
 	err = xml.Unmarshal([]byte(data), &b)
 	if err != nil {
 		return err
 	}
-	b.Path = filepath.ToSlash(path)
-
-	//Looking for cover.
-	for _, f := range r.File {
-		if (filepath.Ext(f.FileHeader.Name) == ".jpg" ||
-			filepath.Ext(f.FileHeader.Name) == ".jpeg" ||
-			filepath.Ext(f.FileHeader.Name) == ".png") &&
-			strings.Contains(f.FileHeader.Name, "cover") {
-			//Copying cover outside of .zip with the new name
-			b.Cover = strings.TrimSuffix(b.Path, ".epub") + filepath.Ext(f.FileHeader.Name)
-			cov, err := os.Create(b.Cover)
-			if err != nil {
-				return err
-			}
-			reader, err := f.Open()
-			if err != nil {
-				return err
-			}
-			_, err = io.Copy(cov, reader)
-			if err != nil {
-				return err
-			}
-			if reader.Close() != nil {
-				return err
-			}
+	Path := filepath.ToSlash(path)
+	
+	//Cover quest
+	//Searching for <meta name="cover" id="..." \> to get needed item id
+	id := ""
+	cover := ""
+	for _, m := range b.Meta {
+		if m.Name == "cover" {
+			id = m.Content
 			break
 		}
 	}
-	bc.books = append(bc.books, b)
+	//Looking for item with newfound id to get reference to the image itself
+	for _, i := range b.Items {
+		if i.Id == id {
+			cover = i.Ref
+			break
+		}
+	}
+	//Go on if found image path
+	if cover != "" {
+		//Image path in .opf file is relative
+		if filepath.Dir(c.Path.P) != "." {
+			cover = filepath.Dir(c.Path.P) + "/" + cover
+		}
+		//Looking for image
+		var cont *zip.File
+		for _, f := range r.File {
+			if f.FileHeader.Name == cover {
+				cont = f
+				break
+			}
+		}
+		if cont == nil {
+			return errors.New(cover + " not found")
+		}
+		//Copying cover outside of .zip with the new name
+		cover = strings.TrimSuffix(Path, ".epub") + filepath.Ext(cont.FileHeader.Name)
+		cov, err := os.Create(cover)
+		if err != nil {
+			return err
+		}
+		reader, err := cont.Open()
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(cov, reader)
+		if err != nil {
+			return err
+		}
+		if reader.Close() != nil {
+			return err
+		}
+	}
+			
+	bc.books = append(bc.books, Book{b.Title, b.Creator, b.Subject, b.Description, b.Language, Path, cover})
 	log.Println(b.Title)
 	return nil
 }
